0808f85e8faa697e7d0eb56ea1fb6de95268b51c
Fernando Crespo <fernando82@gmail.com>
2015-08-30 23:17:20 -0300

    Fix formatting

--- fuzzyclock/src/test/java/in/crespo/fuzzyclock/FuzzyClockTests.java
+++ fuzzyclock/src/test/java/in/crespo/fuzzyclock/FuzzyClockTests.java
@@ -14,20 +14,17 @@ import junit.framework.TestSuite;
  */
 public class FuzzyClockTests extends TestCase {
 
-    public FuzzyClockTests(String testName)
-    {
-        super(testName);
-    }
-    
-    public static Test suite()
-    {
-        return new TestSuite(FuzzyClockTests.class);
-    }
-    
+	public FuzzyClockTests(String testName) {
+		super(testName);
+	}
+
+	public static Test suite() {
+		return new TestSuite(FuzzyClockTests.class);
+	}
+
 	protected Method getMethod(String methodName) {
 		Method[] ms = FuzzyClock.class.getDeclaredMethods();
 		for (Method m : ms) {
-			
 			if (m.getName().equals(methodName)) {
 				m.setAccessible(true);
 				return m;
@@ -35,8 +32,9 @@ public class FuzzyClockTests extends TestCase {
 		}
 		return null;
 	}
-	
-	public void testRetornaHoraPorExtenso() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+	public void testRetornaHoraPorExtenso()
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method m = getMethod("parseHour");
 
 		assertEquals("meia noite", m.invoke(FuzzyClock.class, 0));
@@ -46,7 +44,8 @@ public class FuzzyClockTests extends TestCase {
 		// FIXME: Precisa verificar as outras horas
 	}
 
-	public void testRetorna_A_ParaHorasNoSingular() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void testRetorna_A_ParaHorasNoSingular()
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method m = getMethod("parseJunction");
 
 		List<Integer> list = Arrays.asList(0, 1, 13, 24);
@@ -56,7 +55,8 @@ public class FuzzyClockTests extends TestCase {
 		}
 	}
 
-	public void testRetorna_As_ParaHorasNoPlural() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void testRetorna_As_ParaHorasNoPlural()
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method m = getMethod("parseJunction");
 
 		List<Integer> list = Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
@@ -66,13 +66,15 @@ public class FuzzyClockTests extends TestCase {
 		}
 	}
 
-	public void testRetorna_O_ParaMeioDia() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void testRetorna_O_ParaMeioDia()
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method m = getMethod("parseJunction");
 
 		assertEquals("o", m.invoke(FuzzyClock.class, 12));
 	}
 
-	public void testRetorna_E_Cinco_ParaMinutosDe3A7() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void testRetorna_E_Cinco_ParaMinutosDe3A7()
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Method m = getMethod("parseMinute");
 
 		List<Integer> list = Arrays.asList(3, 4, 5, 6, 7);
